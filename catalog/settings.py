1d3dceb378af3d4d9fcae84f5cdcaa5f6c97729a
KauaneDelvoss <delvoss.ribas@gmail.com>
2022-12-15 07:34:36 -0500

    comment settings.DATABASE.env

--- catalog/settings.py
+++ catalog/settings.py
@@ -147,7 +147,7 @@ environ.Env.read_env((os.path.join(BASE_DIR, '.env')))
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
-DATABASES = {'default': env.db()}
-
+# DATABASES = {'default': env.db()}
+ENGINE = env('ENGINE')
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
